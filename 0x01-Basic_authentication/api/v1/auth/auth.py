1ec0e90358aa94a360b03307513d19e36159a310
zyambo <mightypz2@gmail.com>
2022-07-26 11:11:37 +0200

    update require_auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,6 +10,14 @@ class Auth():
         """ public method
             Return: False
         """
+        if path is None or excluded_paths is None:
+            return True
+        if not excluded_paths:
+            return True
+        if path[-1] != "/":
+            path = path + "/"
+            if path not in excluded_paths:
+                return True
         return False
 
     def authorization_header(self, request=None) -> str:
