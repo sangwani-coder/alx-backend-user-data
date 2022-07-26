20e4659285ad492be44413f39a38a6b677c4f5c6
zyambo <mightypz2@gmail.com>
2022-07-26 13:13:45 +0200

    add Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,16 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """ inherit Auth"""
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """ extracts Base64"""
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        space = ' '
+        if space in authorization_header:
+            auth = authorization_header.split()
+            if auth[0] != "Basic":
+                return None
+            return auth[1]
