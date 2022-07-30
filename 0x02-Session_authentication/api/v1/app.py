af841a03d86c23bc3faf992ad938d4c5a1cfc2ba
zyambo <mightypz2@gmail.com>
2022-07-30 17:13:20 +0200

    [FIX #3] pycodestyle

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -37,8 +37,8 @@ def before_request_func():
                 '/api/v1/auth_session/login/']
 
     if auth is not None and auth.require_auth(request.path, exl_path) is True:
-        if auth.authorization_header(request) and \
-        auth.session_cookie(request):
+        if auth.authorization_header(request) is None and \
+                        auth.session_cookie(request) is None:
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
