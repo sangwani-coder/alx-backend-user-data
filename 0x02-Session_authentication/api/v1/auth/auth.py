21a7334bb7a4fed505957ace57b1b0f4d087783b
zyambo <mightypz2@gmail.com>
2022-07-30 14:22:05 +0000

    [FIX] pycodestyle

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -47,15 +47,8 @@ class Auth():
         Return - None
         """
         return None
-<<<<<<< HEAD
 
     def session_cookie(self, request=None):
         """ Return cookie from request"""
         if request is None:
             return None
-
-
-
-
-=======
->>>>>>> d5f6981c8fdd1357167365069e5135f396dd15ae
